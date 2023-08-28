f6418ac5de29a511f462cb9fb9d3b88fe48994a7
CarlosAlamillaPerez <carlosalamillap@gmail.com>
2023-08-28 13:57:27 -0600

    Mi Portafolio Virtual versión 0.1.6

--- public/build.sh
+++ public/build.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 
-# Instalar PHP 8.2
-...
+set -x
 
+# Instalar PHP 
+echo "Installing PHP..."
+
+# Composer
+echo "Installing Composer..."
 composer install
+
+# NPM 
+echo "Installing NPM dependencies..."
 npm install
 npm run prod
 
+# Laravel
+echo "Running Laravel post-install..."
 php artisan config:cache
-php artisan view:cache 
+php artisan view:cache
 php artisan route:cache
